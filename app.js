405cb7425b2b441302d6285c81bdd3fac4889e4c
Kaushik-jagana <jaganakaushik@gmail.com>
2022-12-31 16:42:11 +0530

    gif

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ console.log("Welcome to Spotify")
 let songindex=0;
 let audioElement= new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
+let gif=document.getElementById('gif');
 
 masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
         masterPlay.classList.remove('fa-circle-play');
         masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterPlay.classList.remove('fa-circle-pause');
         masterPlay.classList.add('fa-circle-play'); 
+        gif.style.opacity=0;
     }
 })
