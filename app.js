88601600c5b84dffeb173a50f8bdfde5f960e007
Kaushik-jagana <jaganakaushik@gmail.com>
2022-12-31 16:38:35 +0530

    Play && Pause

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Welcome to Spotify")
+
+let songindex=0;
+let audioElement= new Audio("songs/1.mp3");
+let masterPlay = document.getElementById('masterPlay');
+
+masterPlay.addEventListener('click',()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play'); 
+    }
+})
